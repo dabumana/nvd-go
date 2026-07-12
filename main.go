cbe3f0f80ff77a55ee77087e643e66be18eadddf
dabumana <dabumana>
2026-07-12 03:04:07 -0400

    Update UT

--- main.go
+++ main.go
@@ -12,13 +12,15 @@ import (
 )
 
 const (
-	DefaultBaseURL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
-	DefaultTimeout = 30 * time.Second
+	DefaultCVEBaseURL = "services.nvd.nist.gov/rest/json/cves/2.0"
+	DefaultCPEBaseURL = "services.nvd.nist.gov/rest/json/cpes/2.0"
+	DefaultTimeout    = 30 * time.Second
 )
 
 type Client struct {
 	httpClient *http.Client
-	baseURL    string
+	cveBaseURL string
+	cpeBaseURL string
 	apiKey     string
 }
 
@@ -28,8 +30,12 @@ func WithHTTPClient(c *http.Client) Option {
 	return func(cl *Client) { cl.httpClient = c }
 }
 
-func WithBaseURL(baseURL string) Option {
-	return func(cl *Client) { cl.baseURL = baseURL }
+func WithCVEBaseURL(baseURL string) Option {
+	return func(cl *Client) { cl.cveBaseURL = baseURL }
+}
+
+func WithCPEBaseURL(baseURL string) Option {
+	return func(cl *Client) { cl.cpeBaseURL = baseURL }
 }
 
 func WithAPIKey(key string) Option {
@@ -39,7 +45,8 @@ func WithAPIKey(key string) Option {
 func NewClient(opts ...Option) *Client {
 	c := &Client{
 		httpClient: &http.Client{Timeout: DefaultTimeout},
-		baseURL:    DefaultBaseURL,
+		cveBaseURL: DefaultCVEBaseURL,
+		cpeBaseURL: DefaultCPEBaseURL,
 	}
 	for _, opt := range opts {
 		opt(c)
@@ -48,32 +55,46 @@ func NewClient(opts ...Option) *Client {
 }
 
 type CVEResponse struct {
-	ResultsPerPage int            `json:"resultsPerPage"`
-	StartIndex     int            `json:"startIndex"`
-	TotalResults   int            `json:"totalResults"`
-	Format         string         `json:"format"`
-	Version        string         `json:"version"`
-	Timestamp      string         `json:"timestamp"`
-	Vulnerabilities []VulnWrapper `json:"vulnerabilities"`
+	ResultsPerPage  int            `json:"resultsPerPage"`
+	StartIndex      int            `json:"startIndex"`
+	TotalResults    int            `json:"totalResults"`
+	Format          string         `json:"format"`
+	Version         string         `json:"version"`
+	Timestamp       string         `json:"timestamp"`
+	Vulnerabilities []VulnWrapper  `json:"vulnerabilities"`
+}
+
+type CPEResponse struct {
+	ResultsPerPage int          `json:"resultsPerPage"`
+	StartIndex     int          `json:"startIndex"`
+	TotalResults   int          `json:"totalResults"`
+	Format         string       `json:"format"`
+	Version        string       `json:"version"`
+	Timestamp      string       `json:"timestamp"`
+	Products       []CPEWrapper `json:"products"`
 }
 
 type VulnWrapper struct {
 	CVE CVE `json:"cve"`
 }
 
+type CPEWrapper struct {
+	CPE CPE `json:"cpe"`
+}
+
 type CVE struct {
-	ID               string                 `json:"id"`
-	SourceIdentifier string                 `json:"sourceIdentifier"`
-	Published        string                 `json:"published"`
-	LastModified     string                 `json:"lastModified"`
-	VulnStatus       string                 `json:"vulnStatus"`
-	Descriptions     []Description          `json:"descriptions"`
-	Metrics          Metrics                `json:"metrics"`
-	References       []Reference            `json:"references"`
-	Configurations   []Configuration        `json:"configurations"`
-	Weaknesses       []Weakness             `json:"weaknesses"`
-	CWEs             []CWE                  `json:"cwe"`
-	CPEs             []CPE                  `json:"cpe"`
+	ID              string         `json:"id"`
+	SourceIdentifier string         `json:"sourceIdentifier"`
+	Published       string         `json:"published"`
+	LastModified    string         `json:"lastModified"`
+	VulnStatus      string         `json:"vulnStatus"`
+	Descriptions    []Description   `json:"descriptions"`
+	Metrics         Metrics        `json:"metrics"`
+	References      []Reference    `json:"references"`
+	Configurations  []Configuration `json:"configurations"`
+	Weaknesses      []Weakness     `json:"weaknesses"`
+	CWEs            []CWE          `json:"cwe"`
+	CPEs            []CPE          `json:"cpe"`
 }
 
 type Description struct {
@@ -89,17 +110,17 @@ type Metrics struct {
 }
 
 type CVSSV2 struct {
-	Source   string  `json:"source"`
-	Type     string  `json:"type"`
-	CVSSData CVSSV2Data `json:"cvssData"`
-	BaseSeverity string `json:"baseSeverity"`
-	ExploitabilityScore float64 `json:"exploitabilityScore"`
-	ImpactScore float64 `json:"impactScore"`
-	ACInsufInfo bool `json:"acInsufInfo"`
-	ObtainAllPrivilege bool `json:"obtainAllPrivilege"`
-	ObtainUserPrivilege bool `json:"obtainUserPrivilege"`
-	ObtainOtherPrivilege bool `json:"obtainOtherPrivilege"`
-	UserInteractionRequired bool `json:"userInteractionRequired"`
+	Source                 string      `json:"source"`
+	Type                   string      `json:"type"`
+	CVSSData               CVSSV2Data `json:"cvssData"`
+	BaseSeverity           string      `json:"baseSeverity"`
+	ExploitabilityScore    float64     `json:"exploitabilityScore"`
+	ImpactScore            float64     `json:"impactScore"`
+	ACInsufInfo            bool        `json:"acInsufInfo"`
+	ObtainAllPrivilege     bool        `json:"obtainAllPrivilege"`
+	ObtainUserPrivilege    bool        `json:"obtainUserPrivilege"`
+	ObtainOtherPrivilege   bool        `json:"obtainOtherPrivilege"`
+	UserInteractionRequired bool       `json:"userInteractionRequired"`
 }
 
 type CVSSV2Data struct {
@@ -115,12 +136,12 @@ type CVSSV2Data struct {
 }
 
 type CVSSV31 struct {
-	Source   string    `json:"source"`
-	Type     string    `json:"type"`
-	CVSSData CVSSV31Data `json:"cvssData"`
-	BaseSeverity string `json:"baseSeverity"`
-	ExploitabilityScore float64 `json:"exploitabilityScore"`
-	ImpactScore float64 `json:"impactScore"`
+	Source               string       `json:"source"`
+	Type                 string       `json:"type"`
+	CVSSData             CVSSV31Data  `json:"cvssData"`
+	BaseSeverity         string       `json:"baseSeverity"`
+	ExploitabilityScore  float64      `json:"exploitabilityScore"`
+	ImpactScore          float64      `json:"impactScore"`
 }
 
 type CVSSV31Data struct {
@@ -139,12 +160,12 @@ type CVSSV31Data struct {
 }
 
 type CVSSV3 struct {
-	Source   string    `json:"source"`
-	Type     string    `json:"type"`
-	CVSSData CVSSV3Data `json:"cvssData"`
-	BaseSeverity string `json:"baseSeverity"`
-	ExploitabilityScore float64 `json:"exploitabilityScore"`
-	ImpactScore float64 `json:"impactScore"`
+	Source               string      `json:"source"`
+	Type                 string      `json:"type"`
+	CVSSData             CVSSV3Data  `json:"cvssData"`
+	BaseSeverity         string      `json:"baseSeverity"`
+	ExploitabilityScore  float64     `json:"exploitabilityScore"`
+	ImpactScore          float64     `json:"impactScore"`
 }
 
 type CVSSV3Data struct {
@@ -163,27 +184,27 @@ type CVSSV3Data struct {
 }
 
 type CVSSV40 struct {
-	Source   string    `json:"source"`
-	Type     string    `json:"type"`
+	Source   string      `json:"source"`
+	Type     string      `json:"type"`
 	CVSSData CVSSV40Data `json:"cvssData"`
 }
 
 type CVSSV40Data struct {
-	Version               string  `json:"version"`
-	VectorString          string  `json:"vectorString"`
-	AttackVector          string  `json:"attackVector"`
-	AttackComplexity      string  `json:"attackComplexity"`
-	AttackRequirements    string  `json:"attackRequirements"`
-	PrivilegesRequired    string  `json:"privilegesRequired"`
-	UserInteraction       string  `json:"userInteraction"`
-	VulnConfidentialityImpact string `json:"vulnConfidentialityImpact"`
-	VulnIntegrityImpact   string  `json:"vulnIntegrityImpact"`
-	VulnAvailabilityImpact string  `json:"vulnAvailabilityImpact"`
-	SubConfidentialityImpact string `json:"subConfidentialityImpact"`
-	SubIntegrityImpact    string  `json:"subIntegrityImpact"`
-	SubAvailabilityImpact string  `json:"subAvailabilityImpact"`
-	BaseScore             float64 `json:"baseScore"`
-	BaseSeverity          string  `json:"baseSeverity"`
+	Version                   string  `json:"version"`
+	VectorString              string  `json:"vectorString"`
+	AttackVector              string  `json:"attackVector"`
+	AttackComplexity          string  `json:"attackComplexity"`
+	AttackRequirements        string  `json:"attackRequirements"`
+	PrivilegesRequired        string  `json:"privilegesRequired"`
+	UserInteraction           string  `json:"userInteraction"`
+	VulnConfidentialityImpact  string  `json:"vulnConfidentialityImpact"`
+	VulnIntegrityImpact       string  `json:"vulnIntegrityImpact"`
+	VulnAvailabilityImpact    string  `json:"vulnAvailabilityImpact"`
+	SubConfidentialityImpact  string  `json:"subConfidentialityImpact"`
+	SubIntegrityImpact        string  `json:"subIntegrityImpact"`
+	SubAvailabilityImpact     string  `json:"subAvailabilityImpact"`
+	BaseScore                 float64 `json:"baseScore"`
+	BaseSeverity              string  `json:"baseSeverity"`
 }
 
 type Reference struct {
@@ -193,15 +214,15 @@ type Reference struct {
 }
 
 type Configuration struct {
-	ID      string        `json:"id"`
-	Nodes   []ConfigNode  `json:"nodes"`
-	Operator string       `json:"operator"`
+	ID       string      `json:"id"`
+	Nodes    []ConfigNode `json:"nodes"`
+	Operator string      `json:"operator"`
 }
 
 type ConfigNode struct {
-	Operator string        `json:"operator"`
-	Negate   bool          `json:"negate"`
-	CPEMatch []CPEMatch    `json:"cpeMatch"`
+	Operator string    `json:"operator"`
+	Negate   bool      `json:"negate"`
+	CPEMatch []CPEMatch `json:"cpeMatch"`
 }
 
 type CPEMatch struct {
@@ -215,49 +236,47 @@ type CPEMatch struct {
 }
 
 type Weakness struct {
-	Source      string            `json:"source"`
-	Type        string            `json:"type"`
-	Description []Description     `json:"description"`
+	Source      string        `json:"source"`
+	Type        string        `json:"type"`
+	Description []Description `json:"description"`
 }
 
 type CWE struct {
-	ID          string        `json:"id"`
-	Name        string        `json:"name"`
-	Description string        `json:"description"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 type CPE struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Deprecated  bool   `json:"deprecated"`
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	Deprecated bool   `json:"deprecated"`
 }
 
 type Filter struct {
-	CveID              string
-	CpeName            string
-	CvssV2Severity     string
-	CvssV3Severity     string
-	CvssV4Severity     string
-	CweID              string
-	KeywordSearch      string
-	KeywordExactMatch  bool
-	PubStartDate       string
-	PubEndDate         string
-	LastModStartDate   string
-	LastModEndDate     string
-	HasKev             *bool
-	HasCertAlerts      *bool
-	HasCertNotes       *bool
-	IsVulnerable       *bool
-	ResultsPerPage     int
-	StartIndex         int
+	CveID             string
+	CpeName           string
+	CvssV2Severity    string
+	CvssV3Severity    string
+	CvssV4Severity    string
+	CweID             string
+	KeywordSearch     string
+	KeywordExactMatch bool
+	PubStartDate      string
+	PubEndDate        string
+	LastModStartDate  string
+	LastModEndDate    string
+	HasKev            *bool
+	HasCertAlerts     *bool
+	HasCertNotes      *bool
+	IsVulnerable      *bool
+	ResultsPerPage    int
+	StartIndex        int
 }
 
 func (c *Client) GetCVE(cveID string) (*CVE, error) {
 	resp, err := c.SearchCves(Filter{CveID: cveID, ResultsPerPage: 1})
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 	if len(resp.Vulnerabilities) == 0 {
 		return nil, fmt.Errorf("CVE %s not found", cveID)
 	}
@@ -266,149 +285,123 @@ func (c *Client) GetCVE(cveID string) (*CVE, error) {
 
 func (c *Client) SearchCves(filter Filter) (*CVEResponse, error) {
 	params := url.Values{}
+	if filter.CveID != "" { params.Set("cveId", filter.CveID) }
+	if filter.CpeName != "" { params.Set("cpeName", filter.CpeName) }
+	if filter.CvssV2Severity != "" { params.Set("cvssV2Severity", filter.CvssV2Severity) }
+	if filter.CvssV3Severity != "" { params.Set("cvssV3Severity", filter.CvssV3Severity) }
+	if filter.CvssV4Severity != "" { params.Set("cvssV4Severity", filter.CvssV4Severity) }
+	if filter.CweID != "" { params.Set("cweId", filter.CweID) }
+	if filter.KeywordSearch != "" { params.Set("keywordSearch", filter.KeywordSearch) }
+	if filter.KeywordExactMatch { params.Set("keywordExactMatch", "true") }
+	if filter.PubStartDate != "" { params.Set("pubStartDate", filter.PubStartDate) }
+	if filter.PubEndDate != "" { params.Set("pubEndDate", filter.PubEndDate) }
+	if filter.LastModStartDate != "" { params.Set("lastModStartDate", filter.LastModStartDate) }
+	if filter.LastModEndDate != "" { params.Set("lastModEndDate", filter.LastModEndDate) }
+	if filter.HasKev != nil { params.Set("hasKev", strconv.FormatBool(*filter.HasKev)) }
+	if filter.HasCertAlerts != nil { params.Set("hasCertAlerts", strconv.FormatBool(*filter.HasCertAlerts)) }
+	if filter.HasCertNotes != nil { params.Set("hasCertNotes", strconv.FormatBool(*filter.HasCertNotes)) }
+	if filter.IsVulnerable != nil { params.Set("isVulnerable", strconv.FormatBool(*filter.IsVulnerable)) }
+	if filter.ResultsPerPage > 0 { params.Set("resultsPerPage", strconv.Itoa(filter.ResultsPerPage)) }
+	if filter.StartIndex > 0 { params.Set("startIndex", strconv.Itoa(filter.StartIndex)) }
 
-	if filter.CveID != "" {
-		params.Set("cveId", filter.CveID)
-	}
-	if filter.CpeName != "" {
-		params.Set("cpeName", filter.CpeName)
-	}
-	if filter.CvssV2Severity != "" {
-		params.Set("cvssV2Severity", filter.CvssV2Severity)
-	}
-	if filter.CvssV3Severity != "" {
-		params.Set("cvssV3Severity", filter.CvssV3Severity)
-	}
-	if filter.CvssV4Severity != "" {
-		params.Set("cvssV4Severity", filter.CvssV4Severity)
-	}
-	if filter.CweID != "" {
-		params.Set("cweId", filter.CweID)
-	}
-	if filter.KeywordSearch != "" {
-		params.Set("keywordSearch", filter.KeywordSearch)
-	}
-	if filter.KeywordExactMatch {
-		params.Set("keywordExactMatch", "true")
-	}
-	if filter.PubStartDate != "" {
-		params.Set("pubStartDate", filter.PubStartDate)
-	}
-	if filter.PubEndDate != "" {
-		params.Set("pubEndDate", filter.PubEndDate)
-	}
-	if filter.LastModStartDate != "" {
-		params.Set("lastModStartDate", filter.LastModStartDate)
-	}
-	if filter.LastModEndDate != "" {
-		params.Set("lastModEndDate", filter.LastModEndDate)
-	}
-	if filter.HasKev != nil {
-		params.Set("hasKev", strconv.FormatBool(*filter.HasKev))
-	}
-	if filter.HasCertAlerts != nil {
-		params.Set("hasCertAlerts", strconv.FormatBool(*filter.HasCertAlerts))
-	}
-	if filter.HasCertNotes != nil {
-		params.Set("hasCertNotes", strconv.FormatBool(*filter.HasCertNotes))
-	}
-	if filter.IsVulnerable != nil {
-		params.Set("isVulnerable", strconv.FormatBool(*filter.IsVulnerable))
-	}
-	if filter.ResultsPerPage > 0 {
-		params.Set("resultsPerPage", strconv.Itoa(filter.ResultsPerPage))
-	}
-	if filter.StartIndex > 0 {
-		params.Set("startIndex", strconv.Itoa(filter.StartIndex))
-	}
-
-	reqURL := c.baseURL
-	if len(params) > 0 {
-		reqURL += "?" + params.Encode()
-	}
+	reqURL := c.cveBaseURL
+	if len(params) > 0 { reqURL += "?" + params.Encode() }
 
 	req, err := http.NewRequest("GET", reqURL, nil)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 	req.Header.Set("Accept", "application/json")
-	if c.apiKey != "" {
-		req.Header.Set("apiKey", c.apiKey)
-	}
+	if c.apiKey != "" { req.Header.Set("apiKey", c.apiKey) }
 
 	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 	defer resp.Body.Close()
-
 	if resp.StatusCode != http.StatusOK {
 		body, _ := io.ReadAll(resp.Body)
 		return nil, fmt.Errorf("HTTP %d: %s", resp.StatusCode, string(body))
 	}
 
 	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 
 	var result CVEResponse
-	if err := json.Unmarshal(body, &result); err != nil {
-		return nil, err
+	if err := json.Unmarshal(body, &result); err != nil { return nil, err }
+	return &result, nil
+}
+
+func (c *Client) SearchCpes(filter Filter) (*CPEResponse, error) {
+	params := url.Values{}
+	if filter.CpeName != "" { params.Set("cpeName", filter.CpeName) }
+	if filter.ResultsPerPage > 0 { params.Set("resultsPerPage", strconv.Itoa(filter.ResultsPerPage)) }
+	if filter.StartIndex > 0 { params.Set("startIndex", strconv.Itoa(filter.StartIndex)) }
+
+	reqURL := c.cpeBaseURL
+	if len(params) > 0 { reqURL += "?" + params.Encode() }
+
+	req, err := http.NewRequest("GET", reqURL, nil)
+	if err != nil { return nil, err }
+	req.Header.Set("Accept", "application/json")
+	if c.apiKey != "" { req.Header.Set("apiKey", c.apiKey) }
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil { return nil, err }
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("HTTP %d: %s", resp.StatusCode, string(body))
 	}
 
+	body, err := io.ReadAll(resp.Body)
+	if err != nil { return nil, err }
+
+	var result CPEResponse
+	if err := json.Unmarshal(body, &result); err != nil { return nil, err }
 	return &result, nil
 }
 
 func (c *Client) SearchByKeyword(keyword string) ([]CVE, error) {
 	resp, err := c.SearchCves(Filter{KeywordSearch: keyword})
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 	return extractCVEs(resp), nil
 }
 
 func (c *Client) SearchByCPE(cpeName string) ([]CVE, error) {
 	resp, err := c.SearchCves(Filter{CpeName: cpeName})
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 	return extractCVEs(resp), nil
+}
+
+func (c *Client) SearchCPEs(cpeName string) ([]CPE, error) {
+	resp, err := c.SearchCpes(Filter{CpeName: cpeName})
+	if err != nil { return nil, err }
+	out := make([]CPE, len(resp.Products))
+	for i, p := range resp.Products {
+		out[i] = p.CPE
+	}
+	return out, nil
 }
 
 func (c *Client) SearchByCWE(cweID string) ([]CVE, error) {
 	resp, err := c.SearchCves(Filter{CweID: cweID})
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 	return extractCVEs(resp), nil
 }
 
 func (c *Client) GetByDateRange(pubStart, pubEnd string) ([]CVE, error) {
-	resp, err := c.SearchCves(Filter{
-		PubStartDate: pubStart,
-		PubEndDate:   pubEnd,
-	})
-	if err != nil {
-		return nil, err
-	}
+	resp, err := c.SearchCves(Filter{PubStartDate: pubStart, PubEndDate: pubEnd})
+	if err != nil { return nil, err }
 	return extractCVEs(resp), nil
 }
 
 func (c *Client) GetBySeverity(severity string) ([]CVE, error) {
 	resp, err := c.SearchCves(Filter{CvssV3Severity: severity})
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 	return extractCVEs(resp), nil
 }
 
 func (c *Client) GetKevCatalog() ([]CVE, error) {
 	kev := true
 	resp, err := c.SearchCves(Filter{HasKev: &kev})
-	if err != nil {
-		return nil, err
-	}
+	if err != nil { return nil, err }
 	return extractCVEs(resp), nil
 }
 
@@ -419,25 +412,16 @@ func (c *Client) GetAll(limit int) ([]CVE, error) {
 	if limit > 0 && limit < perPage {
 		perPage = limit
 	}
-
 	for {
-		resp, err := c.SearchCves(Filter{
-			ResultsPerPage: perPage,
-			StartIndex:     startIndex,
-		})
-		if err != nil {
-			return nil, err
-		}
-
+		resp, err := c.SearchCves(Filter{ResultsPerPage: perPage, StartIndex: startIndex})
+		if err != nil { return nil, err }
 		cves := extractCVEs(resp)
 		all = append(all, cves...)
-
 		if len(all) >= resp.TotalResults || (limit > 0 && len(all) >= limit) {
 			break
 		}
 		startIndex += len(cves)
 	}
-
 	if limit > 0 && len(all) > limit {
 		all = all[:limit]
 	}
---
+++ main_test.go
@@ -0,0 +1,305 @@
+package nvd
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+)
+
+const (
+	testCVEBaseURL = "https://services.nvd.nist.gov/rest/json/cves/2.0"
+	testCPEBaseURL = "https://services.nvd.nist.gov/rest/json/cpes/2.0"
+)
+
+func liveClient() *Client {
+	return NewClient(
+		WithCVEBaseURL(testCVEBaseURL),
+		WithCPEBaseURL(testCPEBaseURL),
+		WithHTTPClient(&http.Client{Timeout: 60 * time.Second}),
+	)
+}
+
+func TestNewClientAndOptions(t *testing.T) {
+	c := NewClient(
+		WithHTTPClient(&http.Client{Timeout: 10 * time.Second}),
+		WithCVEBaseURL(testCVEBaseURL),
+		WithCPEBaseURL(testCPEBaseURL),
+		WithAPIKey("test-key"),
+	)
+
+	if c.httpClient == nil {
+		t.Fatal("httpClient is nil")
+	}
+	if c.cveBaseURL != testCVEBaseURL {
+		t.Fatalf("unexpected cveBaseURL: %s", c.cveBaseURL)
+	}
+	if c.cpeBaseURL != testCPEBaseURL {
+		t.Fatalf("unexpected cpeBaseURL: %s", c.cpeBaseURL)
+	}
+	if c.apiKey != "test-key" {
+		t.Fatalf("unexpected apiKey: %s", c.apiKey)
+	}
+}
+
+func TestSearchCves(t *testing.T) {
+	c := liveClient()
+
+	resp, err := c.SearchCves(Filter{
+		CveID:          "CVE-2023-4863",
+		ResultsPerPage: 1,
+	})
+	if err != nil {
+		t.Fatalf("SearchCves failed: %v", err)
+	}
+	if resp == nil {
+		t.Fatal("nil response")
+	}
+	if len(resp.Vulnerabilities) == 0 {
+		t.Fatal("expected vulnerabilities")
+	}
+	if resp.Vulnerabilities[0].CVE.ID == "" {
+		t.Fatal("expected CVE ID")
+	}
+}
+
+func TestGetCVE(t *testing.T) {
+	c := liveClient()
+
+	cve, err := c.GetCVE("CVE-2023-4863")
+	if err != nil {
+		t.Fatalf("GetCVE failed: %v", err)
+	}
+	if cve == nil {
+		t.Fatal("nil cve")
+	}
+	if cve.ID != "CVE-2023-4863" {
+		t.Fatalf("unexpected CVE ID: %s", cve.ID)
+	}
+}
+
+func TestSearchCpes(t *testing.T) {
+	c := liveClient()
+
+	resp, err := c.SearchCpes(Filter{
+		ResultsPerPage: 5,
+	})
+	if err != nil {
+		t.Fatalf("SearchCpes failed: %v", err)
+	}
+	if resp == nil {
+		t.Fatal("nil response")
+	}
+	if len(resp.Products) == 0 {
+		t.Fatal("expected products")
+	}
+}
+
+func TestSearchCPEs(t *testing.T) {
+	c := liveClient()
+
+	cpes, err := c.SearchCPEs("")
+	if err != nil {
+		t.Fatalf("SearchCPEs failed: %v", err)
+	}
+	if len(cpes) == 0 {
+		t.Fatal("expected CPEs")
+	}
+}
+
+func TestSearchByKeyword(t *testing.T) {
+	c := liveClient()
+
+	cves, err := c.SearchByKeyword("openssl")
+	if err != nil {
+		t.Fatalf("SearchByKeyword failed: %v", err)
+	}
+	if len(cves) == 0 {
+		t.Fatal("expected CVEs")
+	}
+}
+
+func TestSearchByCPE(t *testing.T) {
+	c := liveClient()
+
+	cves, err := c.SearchByCPE("openssl")
+	if err != nil {
+		t.Fatalf("SearchByCPE failed: %v", err)
+	}
+	if len(cves) == 0 {
+		t.Fatal("expected CVEs")
+	}
+}
+
+func TestSearchByCWE(t *testing.T) {
+	c := liveClient()
+
+	cves, err := c.SearchByCWE("CWE-79")
+	if err != nil {
+		t.Fatalf("SearchByCWE failed: %v", err)
+	}
+	if len(cves) == 0 {
+		t.Fatal("expected CVEs")
+	}
+}
+
+func TestGetByDateRange(t *testing.T) {
+	c := liveClient()
+
+	cves, err := c.GetByDateRange("2023-01-01T00:00:00.000", "2023-01-31T23:59:59.999")
+	if err != nil {
+		t.Fatalf("GetByDateRange failed: %v", err)
+	}
+	if len(cves) == 0 {
+		t.Fatal("expected CVEs")
+	}
+}
+
+func TestGetBySeverity(t *testing.T) {
+	c := liveClient()
+
+	cves, err := c.GetBySeverity("HIGH")
+	if err != nil {
+		t.Fatalf("GetBySeverity failed: %v", err)
+	}
+	if len(cves) == 0 {
+		t.Fatal("expected CVEs")
+	}
+}
+
+func TestGetKevCatalog(t *testing.T) {
+	c := liveClient()
+
+	cves, err := c.GetKevCatalog()
+	if err != nil {
+		t.Fatalf("GetKevCatalog failed: %v", err)
+	}
+	if len(cves) == 0 {
+		t.Fatal("expected CVEs")
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	c := liveClient()
+
+	cves, err := c.GetAll(5)
+	if err != nil {
+		t.Fatalf("GetAll failed: %v", err)
+	}
+	if len(cves) == 0 {
+		t.Fatal("expected CVEs")
+	}
+	if len(cves) > 5 {
+		t.Fatalf("expected at most 5 CVEs, got %d", len(cves))
+	}
+}
+
+func TestExtractCVEs(t *testing.T) {
+	c := liveClient()
+
+	resp, err := c.SearchCves(Filter{
+		CveID:          "CVE-2023-4863",
+		ResultsPerPage: 1,
+	})
+	if err != nil {
+		t.Fatalf("SearchCves failed: %v", err)
+	}
+
+	cves := extractCVEs(resp)
+	if len(cves) != len(resp.Vulnerabilities) {
+		t.Fatalf("expected %d CVEs, got %d", len(resp.Vulnerabilities), len(cves))
+	}
+	if len(cves) == 0 {
+		t.Fatal("expected at least one CVE")
+	}
+}
+
+func TestGetDescription(t *testing.T) {
+	c := liveClient()
+
+	cve, err := c.GetCVE("CVE-2023-4863")
+	if err != nil {
+		t.Fatalf("GetCVE failed: %v", err)
+	}
+
+	desc := GetDescription(cve)
+	if desc == "" {
+		t.Fatal("expected description")
+	}
+}
+
+func TestGetCVSSV3Score(t *testing.T) {
+	c := liveClient()
+
+	cve, err := c.GetCVE("CVE-2023-4863")
+	if err != nil {
+		t.Fatalf("GetCVE failed: %v", err)
+	}
+
+	score := GetCVSSV3Score(cve)
+	if score < 0 {
+		t.Fatalf("invalid score: %v", score)
+	}
+}
+
+func TestGetCVSSV3Severity(t *testing.T) {
+	c := liveClient()
+
+	cve, err := c.GetCVE("CVE-2023-4863")
+	if err != nil {
+		t.Fatalf("GetCVE failed: %v", err)
+	}
+
+	severity := GetCVSSV3Severity(cve)
+	if severity == "" {
+		t.Fatal("expected severity")
+	}
+}
+
+func TestGetCWEs(t *testing.T) {
+	c := liveClient()
+
+	cve, err := c.GetCVE("CVE-2023-4863")
+	if err != nil {
+		t.Fatalf("GetCVE failed: %v", err)
+	}
+
+	cwes := GetCWEs(cve)
+	for _, cwe := range cwes {
+		if !strings.HasPrefix(cwe, "CWE-") {
+			t.Fatalf("unexpected CWE value: %s", cwe)
+		}
+	}
+}
+
+func TestSearchCves_WithAllFilters(t *testing.T) {
+	c := liveClient()
+
+	kev := true
+	certAlerts := false
+	certNotes := false
+	vuln := true
+
+	f := Filter{
+		CveID:             "CVE-2023-4863",
+		CpeName:           "openssl",
+		CvssV2Severity:    "HIGH",
+		CvssV3Severity:    "HIGH",
+		CvssV4Severity:    "HIGH",
+		CweID:             "CWE-79",
+		KeywordSearch:     "openssl",
+		KeywordExactMatch: true,
+		PubStartDate:      "2023-01-01T00:00:00.000",
+		PubEndDate:        "2023-12-31T23:59:59.999",
+		LastModStartDate:  "2023-01-01T00:00:00.000",
+		LastModEndDate:    "2023-12-31T23:59:59.999",
+		HasKev:            &kev,
+		HasCertAlerts:     &certAlerts,
+		HasCertNotes:      &certNotes,
+		IsVulnerable:      &vuln,
+		ResultsPerPage:    1,
+		StartIndex:        1,
+	}
+
+	_, _ = c.SearchCves(f)
+}
